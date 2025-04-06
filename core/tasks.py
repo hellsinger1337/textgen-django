75c2d30266c19b19457254aa412d31c13a047f57
h1 <savely.crylov@yandex.ru>
2025-04-06 20:48:16 +0300

    feat: send email via MailerSend upon successful text generation

--- core/tasks.py
+++ core/tasks.py
@@ -3,14 +3,10 @@ from openai import OpenAI
 from celery import shared_task
 from django.conf import settings
 from .models import Prompt, PromptContent
+from mailersend import emails
 
 @shared_task
 def generate_text_task(prompt_id):
-    """
-    Асинхронная задача для генерации текста
-    через OpenAI. Меняет статус Prompt на PROCESSING,
-    затем на COMPLETED/FAILED.
-    """
     try:
         prompt = Prompt.objects.get(id=prompt_id)
         prompt.status = Prompt.STATUS_PROCESSING
@@ -36,6 +32,28 @@ def generate_text_task(prompt_id):
 
         prompt.status = Prompt.STATUS_COMPLETED
         prompt.save()
+
+        mailer = emails.NewEmail(os.getenv('MAILERSEND_API_KEY'))
+
+        mail_body = {}
+
+        mail_from = {
+            "email": os.getenv('MAILERSEND_FROM_EMAIL'),
+        }
+
+        recipients = [
+            {
+                "email": os.getenv('MAILERSEND_TO_EMAIL'),
+            }
+        ]
+
+        mailer.set_mail_from(mail_from, mail_body)
+        mailer.set_mail_to(recipients, mail_body)
+        mailer.set_subject("Hello!", mail_body)
+        mailer.set_html_content(generated_text, mail_body)
+        mailer.set_plaintext_content(generated_text, mail_body)
+
+        mailer.send(mail_body)
 
     except Exception as e:
         print(f"Error in generate_text_task for Prompt ID={prompt_id}: {e}")
